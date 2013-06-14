1eced00ddcfeae33680dc7a3fb752998e50269fb
Gant <GantMan@gmail.com>
2013-06-14 10:07:12 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "ProMotion", "~> 0.7.5"
---
+++ app/screens/home_screen.rb
@@ -0,0 +1,15 @@
+class HomeScreen < PM::Screen
+	title "Drink Drank Drunk"
+
+	def will_appear
+		#stuff to show
+	end
+
+	def on_load
+		# Load stuff
+	end
+
+	def on_appear
+		#all loaded and visible
+	end
+end
